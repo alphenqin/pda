70ecb0d46d8ac1babe340aa09ae5aafbc89dd73c
alphenqin <alphenqin@163.com>
2025-12-28 16:03:54 +0800

    新增阀门管理

--- app/src/main/java/com/qs/qs5502demo/api/ApiConfig.java
+++ app/src/main/java/com/qs/qs5502demo/api/ApiConfig.java
@@ -12,9 +12,9 @@ public class ApiConfig {
     // 默认服务器地址
     // WMS接口基础地址（10.0.2.2是Android模拟器访问开发电脑localhost的特殊地址）
     // 本地开发环境
-//    private static final String DEFAULT_WMS_BASE_URL = "http://10.0.2.2:8080/api";
+    private static final String DEFAULT_WMS_BASE_URL = "http://10.0.2.2:8080/api";
     // 部署环境
-    private static final String DEFAULT_WMS_BASE_URL = "http://192.168.2.4:9080/api";
+//    private static final String DEFAULT_WMS_BASE_URL = "http://192.168.2.4:9080/api";
     
     // AGV调度系统接口基础地址
     private static final String DEFAULT_AGV_BASE_URL = "http://192.168.2.4:81/pt";
